ded25939b6ffad0162570e26dbab2ce6aa788d9c
anasir3003 <74717035+anasir3003@users.noreply.github.com>
2022-04-29 06:06:54 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     mumbai: {
       url: "https://polygon-mumbai.g.alchemy.com/v2/jQBuyIljmN-7JHkcY18nP0kY_Ys8lyMF",
-      accounts: ["999e9465bf2d580282e0519481caca479a7c353e6e316a60ae656e1c3f32dd31"],
+      accounts: ["#Private Key"],
     }
   }
 };
